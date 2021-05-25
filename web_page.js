0170d78c66b1329e96221ba80e522f15d2b8552b
lowell <lowelljacobs@mail.com>
2021-05-24 18:07:00 -0700

    add codes for adding/remove style sheets

--- web_page.js
+++ web_page.js
@@ -1,6 +1,29 @@
-// content script for adding and removing css in the web page
+// script that runs in the context of the web page
 
 chrome.runtime.onMessage.addListener((request) => {
+  const { active, name, content } = request;
+  const styleSheetElement = document.querySelector(
+    `style[style_name="${name}"]`
+  );
+
+  if (active) {
+    if (styleSheetElement === null) {
+      // create and add style sheet
+
+      const newStyleSheet = document.createElement("style");
+
+      newStyleSheet.setAttribute("style_name", name);
+      newStyleSheet.innerHTML = content;
+
+      document.head.appendChild(newStyleSheet);
+    } else {
+      // modify existing style sheet
+      styleSheetElement.innerHTML = content;
+    }
+  } else if (styleSheetElement !== null) {
+    document.head.removeChild(styleSheetElement);
+  }
+
   // console.log(request);
   // remove stylesheet from webpage
   // if(request.removeCSS)

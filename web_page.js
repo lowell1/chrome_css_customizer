f6dddd917b8236d2d2ac962f63ae4fd6b33f46b7
lowell <lowelljacobs@mail.com>
2021-05-24 17:19:06 -0700

    rename script that runs in the web page

--- web_page.js
+++ web_page.js
@@ -1,12 +1,10 @@
 // content script for adding and removing css in the web page
 
 chrome.runtime.onMessage.addListener((request) => {
-  console.log(request);
-
+  // console.log(request);
   // remove stylesheet from webpage
   // if(request.removeCSS)
   //     console
-
   // add stylesheet to webpage
 });
-console.log("hello world");
+// console.log("hello world");

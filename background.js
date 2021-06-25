506459855cf5aca251f5f22e5fb3e26c4e074c87
lowell <lowelljacobs@mail.com>
2021-06-25 14:21:10 -0700

    remove comments

--- background.js
+++ background.js
@@ -1,13 +1,4 @@
-/*
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.clear();
-  chrome.storage.local.set({ inputs: { darkMode: false } });
-});
-
 // create initial state for inputs
-
-// chrome.storage.local.set({ inputs: { darkMode: false, input2: true } });
-*/
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({
